1717fd508c7e373e8005324fabe4120240718a3b
Keith Garcia <Keith@Keiths-MacBook.local>
2017-06-01 12:39:02 -0400

    update changes

--- src/main/java/com/theironyard/lastProject/controllers/MealController.java
+++ src/main/java/com/theironyard/lastProject/controllers/MealController.java
@@ -1,27 +1,36 @@
 package com.theironyard.lastProject.controllers;
 
 import com.theironyard.lastProject.entities.Meal;
+import com.theironyard.lastProject.entities.User;
+import com.theironyard.lastProject.repositories.MealRepository;
+import com.theironyard.lastProject.repositories.UserRepository;
 import com.theironyard.lastProject.services.MealService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Keith on 5/31/17.
  */
 @RestController
 public class MealController {
+    MealRepository meals;
     MealService mealService;
+    UserRepository users;
 
-    public MealController(MealService mealService) {
+    public MealController(MealService mealService, UserRepository users) {
+        this.users = users;
         this.mealService = mealService;
     }
 
     @RequestMapping(path = "/new-meal", method = RequestMethod.POST) //User user or Meal meal?
     public void saveMeal(@RequestBody Meal meal){ //@requestbody?? //save meal = method name
-        mealService.saveMeal(meal);
+
+        User u = users.findOne(1);
+        mealService.saveMeal(meal, u);
     }
 
-    @RequestMapping(path= "/")
+
+    @RequestMapping(path = "/select-meal/{id}", method = RequestMethod.GET)
+    public Meal getMeal(@PathVariable("id") int id) {
+        return meals.findOne(id);
+    }
 }
--- src/main/java/com/theironyard/lastProject/services/MealService.java
+++ src/main/java/com/theironyard/lastProject/services/MealService.java
@@ -2,6 +2,7 @@ package com.theironyard.lastProject.services;
 
 import com.theironyard.lastProject.entities.Meal;
 import com.theironyard.lastProject.entities.Serving;
+import com.theironyard.lastProject.entities.User;
 import com.theironyard.lastProject.repositories.MealRepository;
 import com.theironyard.lastProject.repositories.ServingRepository;
 import org.springframework.stereotype.Service;
@@ -18,20 +19,9 @@ public class MealService {
         this.meals = mealRepository;
         this.servings = servings;
     }
-//Do I need to findFirstByName or ID?
-//    public void createMeal(String name, boolean cook, String recipe, String pickup, String eta, String category, int servingAmt) {
-//            Meal meal = new Meal(name, cook, recipe, pickup, eta, category);
-//            meals.save(meal);
-//
-//            for(int i = 0; i < servingAmt; i++) {
-//                Serving serving = new Serving(meal);
-//
-//                servings.save(serving);
-//            }
-//        }
 
-
-    public void saveMeal(Meal meal) {
+    public void saveMeal(Meal meal, User u) {
+        meal.setUser(u);
         meals.save(meal);
 
         for(int i = 0; i < meal.getServingCount(); i++){
