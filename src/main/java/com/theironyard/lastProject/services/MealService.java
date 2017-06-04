6564c6a02364906f0b3724adbbd8f3e24e89e4d6
Keith Garcia <Keith@Keiths-MacBook.local>
2017-06-03 21:45:25 -0400

    update

--- src/main/java/com/theironyard/lastProject/services/MealService.java
+++ src/main/java/com/theironyard/lastProject/services/MealService.java
@@ -5,6 +5,7 @@ import com.theironyard.lastProject.entities.Serving;
 import com.theironyard.lastProject.entities.User;
 import com.theironyard.lastProject.repositories.MealRepository;
 import com.theironyard.lastProject.repositories.ServingRepository;
+import com.theironyard.lastProject.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,10 +15,12 @@ import org.springframework.stereotype.Service;
 public class MealService {
     MealRepository meals;
     ServingRepository servings;
+    UserRepository users;
 
-    public MealService(MealRepository mealRepository, ServingRepository servings) {
+    public MealService(MealRepository mealRepository, ServingRepository servings, UserRepository users) {
         this.meals = mealRepository;
         this.servings = servings;
+        this.users = users;
     }
 
     public void saveMeal(Meal meal, User u) {
@@ -26,7 +29,10 @@ public class MealService {
 
         for(int i = 0; i < meal.getServingCount(); i++){
             Serving serving = new Serving(meal);
+            User user = users.findFirstByUsername(u.getUsername());
+            user.setToken(user.getToken() + 1);
             servings.save(serving);
+            users.save(u);
         }
 
     }
@@ -37,15 +43,18 @@ public class MealService {
 
         // see if there are any servings in this meal without an eta (e.g. not taken
         Serving serving = servings.findFirstByMealAndEtaIsNull(meal);
+        User user = users.findFirstByUsername(u.getUsername());
 
         // if there is a serving that matches your criteria,
         if(serving!= null) {
             // update it with requestServing's eta.
             serving.setEta(requestServing.getEta());
+            user.setToken(user.getToken() - 1);
 
             serving.setUserEater(u);
 
             // save the existing serving.
+            users.save(u);
             servings.save(serving);
         }
     }
