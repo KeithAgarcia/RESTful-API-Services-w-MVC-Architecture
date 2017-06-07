8febbf3941c25fae7da9ccd588bf31899feedb29
Keith Garcia <Keith@Keiths-MacBook.local>
2017-06-07 11:40:43 -0400

    fixing cors stuff

---
+++ src/main/java/com/theironyard/lastProject/adapters/WebConfigurerAdapter.java
@@ -0,0 +1,17 @@
+package com.theironyard.lastProject.adapters;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Keith on 6/7/17.
+ */
+@Configuration
+public class WebConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
