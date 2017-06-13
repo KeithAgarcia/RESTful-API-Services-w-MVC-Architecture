d8f9eb5f971b7caca21750ee7b6033a4f85bc92e
Keith Garcia <Keith@Keiths-MacBook.local>
2017-06-13 14:52:08 -0400

    update

--- src/main/java/com/theironyard/lastProject/controllers/UserController.java
+++ src/main/java/com/theironyard/lastProject/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.theironyard.lastProject.entities.User;
 import com.theironyard.lastProject.repositories.MealRepository;
 import com.theironyard.lastProject.repositories.UserRepository;
 import com.theironyard.lastProject.services.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -55,6 +56,14 @@ public class UserController {
     public void rateUser(@PathVariable("id") int id, @RequestBody int rating) {
         User ratee = users.findOne(id);
         userService.rateUser(ratee, rating);
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/userInfo", method = RequestMethod.GET)
+    public User userInfo(){
+        org.springframework.security.core.userdetails.User auth = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User u = users.findFirstByUsername(auth.getUsername());
+        return users.findOne(u.getId());
     }
 
 }
